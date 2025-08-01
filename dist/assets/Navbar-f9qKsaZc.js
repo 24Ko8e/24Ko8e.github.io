619bf477eaebb8edaaba1bcd7a16c09061f5c860
Abhishek <abhishekm942@gmail.com>
2025-08-01 14:36:26 +0530

    build with updated name

--- dist/assets/Navbar-f9qKsaZc.js
+++ dist/assets/Navbar-f9qKsaZc.js
@@ -1,4 +1,4 @@
-import{j as P,r as vt}from"./index-Bv7KViO3.js";import{g as wt,S as bt}from"./ScrollTrigger-CUwdeFQJ.js";/*!
+import{j as P,r as vt}from"./index-0MiR-XQj.js";import{g as wt,S as bt}from"./ScrollTrigger-CUwdeFQJ.js";/*!
  * SplitText 3.13.0
  * https://gsap.com
  *
